8aaf0427da8e3c81d2de7a1dd1ecdd5eef57f517
jims57 <jims57@163.com>
2025-03-27 20:35:26 +0800

    [work]add sync files

---
+++ egs/commonvoice/ASR/pruned_transducer_stateless7_streaming/my-info/combine_diff_accents_to_commonvoice/people-speech/utils/sync_local_files_to_server_docker/aws/sync_convert_parquets_files_into_mp3_and_transcripts_py_to_docker_in_aws.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright    2023-2024  Watchfun Co., Ltd.        (authors: Jimmy Gan)
+#
+# This script syncs the convert_parquets_files_into_mp3_and_transcripts.py file from local machine to AWS server and then to Docker container
+
+# Step 1: Run on local machine to copy the file to AWS server
+echo "Creating directory on AWS server and copying convert_parquets_files_into_mp3_and_transcripts.py..."
+ssh -i /Users/mac/.ssh/aws/aws-icefall.pem ubuntu@3.22.99.8 "mkdir -p ~/icefall/egs/commonvoice/ASR/download/people_speech_ds_1" && \
+scp -i /Users/mac/.ssh/aws/aws-icefall.pem /Users/mac/Documents/GitHub/icefall/egs/commonvoice/ASR/pruned_transducer_stateless7_streaming/my-info/combine_diff_accents_to_commonvoice/people-speech/convert_parquets_files_into_mp3_and_transcripts.py ubuntu@3.22.99.8:~/icefall/egs/commonvoice/ASR/download/people_speech_ds_1/
+
+echo "File copied to AWS server successfully."
+
+# Step 2: Copy the file from AWS server to Docker container
+echo "Copying file from AWS server to Docker container..."
+ssh -i /Users/mac/.ssh/aws/aws-icefall.pem ubuntu@3.22.99.8 "docker cp ~/icefall/egs/commonvoice/ASR/download/people_speech_ds_1/convert_parquets_files_into_mp3_and_transcripts.py 432a764e93ea:/root/icefall/egs/commonvoice/ASR/download/people_speech_ds_1/"
+
+echo "File copied to Docker container successfully."
+echo ""
+echo "If you want to run the script in the Docker container, you can use these commands:"
+echo "1. SSH into the AWS server: ssh -i /Users/mac/.ssh/aws/aws-icefall.pem ubuntu@3.22.99.8"
+echo "2. Access the Docker container: docker exec -it 432a764e93ea bash"
+echo "3. Navigate to the directory: cd /root/icefall/egs/commonvoice/ASR/download/people_speech_ds_1/"
+echo "4. Run the script: bash convert_parquets_files_into_mp3_and_transcripts.py"
